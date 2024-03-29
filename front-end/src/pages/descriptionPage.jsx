c7133b31dd877e210cc545d3b48300adbcafa94c
Neha Padala <saineha.padala@gmail.com>
2024-03-29 19:12:57 -0400

    fixing home page css

--- front-end/src/pages/descriptionPage.jsx
+++ front-end/src/pages/descriptionPage.jsx
@@ -20,21 +20,21 @@ function DescriptionPage() {
         
       <img className='descriptionPage-HeaderImg' src="../../assets/descriptionPage.webp" height="400"/>
       <div className='descriptionPage-TextContainer'>
-        <h2 class="text-center">The Hidden Bias: An Interactive Exploration of AI in Hiring</h2>
+        <h2 className="text-center font-extrabold text-pink-400">The Hidden Bias: An Interactive Exploration of AI in Hiring</h2>
         <p className='descriptionPage-text text-left'>
           
           Algorithms seem efficient, neutral tools.  But what happens when the data used to train AI reflects the all-too-human biases of our world?  
           In this interactive game, you'll step into the role of a hiring manager and experience firsthand how AI can amplify prejudice in the workplace, often with the best of intentions.
 
         </p>
-        <h3>The Challenge</h3>
+        <h3 className="font-bold text-pink-400">The Challenge</h3>
         <p>
           Your decisions matter. As you create job postings, screen resumes, conduct interviews, and make offers, you'll confront subtle -and not so subtle- scenarios where seemingly objective AI tools perpetuate discrimination. 
           Can you build a fair and inclusive team, or will unconscious biases derail your efforts?
         </p>
 
         <section class="feature-section">
-          <h3>Features</h3>
+          <h3 className="font-bold text-pink-400 gap-6">Features</h3>
           <ul class="feature-list">
             <li class="feature-item">
               <p><strong>Real-World Inspiration:</strong> Discover how gender, sexual identity, minority status, age, and even a gap on a resume can lead to biased outcomes, inspired by genuine cases of AI discrimination.</p>
@@ -49,7 +49,7 @@ function DescriptionPage() {
             </li>
           </ul>
         </section>
-        <h3>Why Play?</h3>
+        <h3 className="font-bold text-pink-400">Why Play?</h3>
         <p>
           Whether you're a job seeker, a future manager, or simply someone concerned about technology's impact on society, this game will challenge your assumptions. Play to:
 
@@ -57,7 +57,7 @@ function DescriptionPage() {
           Develop empathy for those impacted by discriminatory algorithms.
           Practice identifying and overcoming bias in a safe environment.
         </p>
-        <button class="bg-indigo-900 hover:border-pink-500 hover:border text-white font-semibold py-2 px-4 rounded">Start Playing, Start Learning!</button>
+        <button class="bg-indigo-900 hover:border-pink-500 hover:border text-white font-semibold py-2 px-4 grid grid-cols-1">Start Playing, Start Learning!</button>
       </div>
 
     </div>
--- front-end/src/pages/startPage.jsx
+++ front-end/src/pages/startPage.jsx
@@ -14,16 +14,18 @@ function StartPage() {
   }, []);
 
   return (
-    <div className="StartPage bg-indigo-950 border-2 border-rose-400 text-neutral-100 rounded-sm" >
+    <div className="StartPage  h-1/4 flex justify-center items-center bg-indigo-950 border-2 border-rose-400 text-neutral-100 rounded-sm" >
         {/*data.map((item) => (
           <li key={item.id}>{item.id}</li>
         ))*/}
+        <img width="400" height="300" src="../../../assets/manager.webp"/>
          <div className='startPage-TextContainer p-8'>
+
           <h2 className='text-xl font-extrabold text-rose-400'>Congratulations!</h2>
           <h3>You have just been promoted to Engineering Manager at Avengers4U.</h3>
           <h3>You are thrilled to take on this new role and lead a team of talented engineers.</h3>
           <br></br>
-          <button className='rounded-sm bg-indigo-700 border-1 border-gray-800 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  hover:bg-pink-600 text-white font-semibold py-2 px-4 border sm-rounded"'><Link to="/game" style={{textDecoration: 'none'}}>Continue...</Link></button>
+          <button className='rounded-sm bg-indigo-600 hover:border-2 hover:border-neutral-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-white font-semibold py-2 px-4 sm-rounded"'><Link to="/game" style={{textDecoration: 'none'}}>Continue...</Link></button>
 
         </div>
         
