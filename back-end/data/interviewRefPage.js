42611fadf558b1a0f2ba0707728ad280dbf63dec
Shaili Kadakia <68572793+shailikadakia@users.noreply.github.com>
2024-04-04 08:22:12 -0400

    updated header for refereces and added citations from Milestone 2 submission

--- back-end/data/interviewRefPage.js
+++ back-end/data/interviewRefPage.js
@@ -1,20 +1,20 @@
 const interviewData = {
   data: [
     {
-      id: 1, 
-      text: `Böhm, S., Linnyk, O., Kohl, J., Weber, T., Teetz, I., Bandurka, K., & Kersting, M. (2020). Analysing gender bias in IT job postings. Proceedings of the 2020 on Computers and People Research Conference, 72–80. https://doi.org/10.1145/3378539.3393862`
+      id: 1,
+      text: ` Sanaullah, N. (2020, March 16). Artificial intelligence in HR: How to address racial biases and algorithmic discrimination in HR? European Network Against Racism. https://www.enar-eu.org/artificial-intelligence-in-hr-how-to-address-racial-biases-and-algorithmic-discrimination-in-hr/`
     },
     {
       id: 2,
-      text: `Bolukbasi, T., Chang, K.-W., Zou, J., Saligrama, V., & Kalai, A. (2016, July 21). Man is to computer programmer as woman is to homemaker? Debiasing word embeddings. arXiv.org. https://arxiv.org/abs/1607.06520`
+      text: `Harwell, D. (2019, October 22). A face-scanning algorithm increasingly decides whether you deserve the job. The Washington Post. https://www.washingtonpost.com/technology/2019/10/22/ai-hiring-face-scanning-algorithm-increasingly-decides-whether-you-deserve-job/`
     },
     {
       id: 3,
-      text: `Carpenter, J. (2015, July 6). Google’s algorithm shows prestigious job ads to men, but not to women. Here’s why that should worry you. The Washington Post. https://www.washingtonpost.com/news/the-intersect/wp/2015/07/06/googles-algorithm-shows-prestigious-job-ads-to-men-but-not-to-women-heres-why-that-should-worry-you/`
+      text: `Beckwith, L., Burnett, M., Wiedenbeck, S., Cook, C., Sorte, S., & Hastings, M. (2005, April 2). Effectiveness of end-user debugging software features. Proceedings of the SIGCHI Conference on Human Factors in Computing Systems. https://www.acm.org/binaries/content/assets/public-policy/aigenderbiaspaper.pdf`
     },
     {
       id: 4,
-      text: ` Tang, S., Zhang, X., Cryan, J., Metzger, M. J., Zheng, H., & Zhao, B. Y. (2017). Gender Bias in the Job Market: A Longitudinal Analysis. Proceedings of the ACM on Human-Computer Interaction, 1(CSCW), 1–19. https://doi.org/10.1145/3134734`
+      text: `Study finds gender and skin-type bias in commercial artificial-intelligence systems. (n.d.). MIT News | Massachusetts Institute of Technology. Retrieved March 13, 2024, from https://news.mit.edu/2018/study-finds-gender-skin-type-bias-artificial-intelligence-systems-0212`
     }
   ]
 }
--- back-end/data/postSelectionRefPage.js
+++ back-end/data/postSelectionRefPage.js
@@ -1,20 +1,16 @@
 const postSelectionData = {
   data: [
     {
-      id: 1, 
-      text: `Böhm, S., Linnyk, O., Kohl, J., Weber, T., Teetz, I., Bandurka, K., & Kersting, M. (2020). Analysing gender bias in IT job postings. Proceedings of the 2020 on Computers and People Research Conference, 72–80. https://doi.org/10.1145/3378539.3393862`
+      id: 1,
+      text: ` Sanaullah, N. (2020, March 16). Artificial intelligence in HR: How to address racial biases and algorithmic discrimination in HR? European Network Against Racism. https://www.enar-eu.org/artificial-intelligence-in-hr-how-to-address-racial-biases-and-algorithmic-discrimination-in-hr/`
     },
     {
       id: 2,
-      text: `Bolukbasi, T., Chang, K.-W., Zou, J., Saligrama, V., & Kalai, A. (2016, July 21). Man is to computer programmer as woman is to homemaker? Debiasing word embeddings. arXiv.org. https://arxiv.org/abs/1607.06520`
+      text: `Nachiappan, A. (2024, January 31). “I thought I was going mad”: Former make-up artist says she lost her job after being marked down by AI tool. Sky. https://news.sky.com/story/companies-increasingly-using-ai-for-recruiting-and-why-it-could-cost-you-your-job-13060905`
     },
     {
       id: 3,
-      text: `Carpenter, J. (2015, July 6). Google’s algorithm shows prestigious job ads to men, but not to women. Here’s why that should worry you. The Washington Post. https://www.washingtonpost.com/news/the-intersect/wp/2015/07/06/googles-algorithm-shows-prestigious-job-ads-to-men-but-not-to-women-heres-why-that-should-worry-you/`
-    },
-    {
-      id: 4,
-      text: ` Tang, S., Zhang, X., Cryan, J., Metzger, M. J., Zheng, H., & Zhao, B. Y. (2017). Gender Bias in the Job Market: A Longitudinal Analysis. Proceedings of the ACM on Human-Computer Interaction, 1(CSCW), 1–19. https://doi.org/10.1145/3134734`
+    text: `Subramaniam, V. (2023, July 8). Apps like Uber and DoorDash use AI to determine pay. Workers say this makes it impossible to predict wages. The Globe and Mail. https://www.theglobeandmail.com/business/article-pay-ai-algorithm-gig-workers-uber/`
     }
   ]
 }
