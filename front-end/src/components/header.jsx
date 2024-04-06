5b54d4c96b873e1a2ee279954d5f0f3831704732
Kriti Sodhi <kritisodhi@Kritis-Air.phub.net.cable.rogers.com>
2024-04-06 09:29:28 -0400

    fixed the hidden header bug

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -7,7 +7,7 @@ function Header () {
                         <img src="../../../assets/logo.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">CS492 Project</span>
                     </a>
-                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                    <div className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <a href="/" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Home</a>
