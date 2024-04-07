213c5a5f35d390a0e3547c005db0f83c0d39e823
juliengo141 <juliengo141@gmail.com>
2024-04-07 15:25:32 -0400

    fix ids

--- back-end/data/screeningRefPage.js
+++ back-end/data/screeningRefPage.js
@@ -5,19 +5,19 @@ const screeningData = {
       text: `Adamovic, A. (2021, June 8). Research Fellow in Management, M. (2021, June 8). What’s in a name? How recruitment discriminates against ‘foreign’ applicants. The Conversation. https://theconversation.com/whats-in-a-name-how-recruitment-discriminates-against-foreign-applicants-160695 `
   },
   {
-      id: 3, 
+      id: 2, 
       text: `Davis, L. M. (2023, June 9). The new age of hiring: AI is changing the game for job seekers. CNET. https://www.cnet.com/tech/features/the-new-age-of-hiring-ai-is-changing-the-game-for-job-seekers/`
   },
   {
-      id: 4,
+      id: 3,
       text: `Liberatore, S. (2023, December 15). Moms are being rejected for jobs and labeled a “liability” by AI resume-screening programs used by 99% of companies, report suggests. https://www.dailymail.co.uk/sciencetech/article-12864913/Moms-rejected-jobs-labeled-liability-AI-resume-screening-programs-used-99-companies-report-suggests.html`
   },
   {
-      id: 5,
+      id: 4,
       text: `O’Brien, M. (2022, May 12). U.S. civil rights enforcers warn employers against biased AI. AP News. https://apnews.com/article/technology-discrimination-artificial-intelligence-e1bcf4a2e7f1b671cbf3a44bc99b3656 `
   },
   {
-      id: 6,
+      id: 5,
       text: `Schmidt, D., & Timko, B. (2023, August 25). EEOC settles first AI hiring bias lawsuit. JD Supra. https://www.jdsupra.com/legalnews/eeoc-settles-first-ai-hiring-bias-6261293/`
   },
   ]
